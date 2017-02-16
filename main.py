b7a022b2854ccf75fa57a1eaff5951b5ad3bf139
Brad Bishop <brad.bishop@me.com>
2017-02-15 23:16:51 -0600

    blog functions

--- main.py
+++ main.py
@@ -39,28 +39,34 @@ class RecentBlogs(Handler):
         content = t.render(blogs=blogs)
         self.response.write(content)
 
-class AddEntry(Handler):
+class NewPost(Handler):
+    def get(self):
+        title = self.request.get("title")
+        entry_blog = self.request.get("entry_blog")
+        error_title = self.request.get("error_title")
+        error_entry = self.request.get("error_entry")
+        t = jinja_env.get_template("newpost.html")
+        content = t.render(title=title, entry_blog=entry_blog, error_title=error_title, error_entry=error_entry)
+        self.response.write(content)
+
     def post(self):
         title = self.request.get("title")
         entry_blog = self.request.get("entry_blog")
-        if title == "" or entry_blog == "":
-            t = jinja_env.get_template("front_page.html")
-            content = t.render(title=title, entry_blog=entry_blog)
-            self.redirect("/newpost?title={0}&entry_blog={1}".format(title,entry_blog))
+        error_title = self.request.get("error_title")
+        error_entry = self.request.get("error_entry")
+        if title == "":
+            error_title = "Don't forget your title!"
+        if entry_blog == "":
+            error_entry = "Don't forget your blog content!"
+        t = jinja_env.get_template("newpost.html")
+        content = t.render(title=title, entry_blog=entry_blog, error_title=error_title, error_entry=error_entry)
+        self.redirect("/newpost?title={0}&entry_blog={1}&error_title={2}&error_entry={3}".format(title,entry_blog,error_title,error_entry))
 
         if title and entry_blog:
             b = Blog(title=title, entry_blog=entry_blog)
             b.put()
 
-        self.redirect("/blog")
-
-class NewPost(Handler):
-    def get(self):
-        title = self.request.get("title")
-        entry_blog = self.request.get("entry_blog")
-        t = jinja_env.get_template("newpost.html")
-        content = t.render(title=title, entry_blog=entry_blog)
-        self.response.write(content)
+            self.redirect("/blog")
 
 class AllBlogs(Handler):
     def get(self):
@@ -72,7 +78,6 @@ class AllBlogs(Handler):
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/newpost', NewPost),
-    ('/add', AddEntry),
     ('/blog', RecentBlogs),
     ('/all', AllBlogs)
 ], debug=True)
